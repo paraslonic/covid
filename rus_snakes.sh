ce55a80ca14bff9ac903400cea28485499378c19
paraslonic <paraslonic@gmail.com>
2020-04-05 12:33:17 +0300

    added common tree from tmp

---
+++ rus_snakes.sh
@@ -0,0 +1,2 @@
+snakemake -s Snakefile -j 100 --use-conda --latency-wait 20 -R randomsnakemake -s Snakefile -j 100 --use-conda --latency-wait 20 -R random
+snakemake -s Snakefile_tree -j 100 --use-conda --latency-wait 20
